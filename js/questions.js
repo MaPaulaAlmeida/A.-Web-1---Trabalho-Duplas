17cda27901111f553ef1b4797582f8c837532105
MaPaulaAlmeida <169.mariapaula@gmail.com>
2024-12-10 01:00:12 +0000

    Version #1 - 45%
    HTML, CSS and part of JavaScript were coded. Still missing the main interaction for the Cinephile Quiz to work.

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Q1",
+    answer: "A2",
+    options: [
+      "A1",
+      "A2",
+      "A3",
+      "A4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Q2",
+    answer: "A4.2",
+    options: [
+      "A1.2",
+      "A2.2",
+      "A3.2",
+      "A4.2"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Q3",
+    answer: "A1.3",
+    options: [
+      "A1.3",
+      "A2.3",
+      "A3.3",
+      "A4.3"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Q4",
+    answer: "A4.4",
+    options: [
+      "A1.4",
+      "A2.4",
+      "A3.4",
+      "A4.4"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Q5",
+    answer: "A1.5",
+    options: [
+      "A1.5",
+      "A2.5",
+      "A3.5",
+      "A4.5"
+    ]
+  },
+];
