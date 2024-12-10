c8856be638b52cbdd3f3688db859bef52f617f1b
MaPaulaAlmeida <169.mariapaula@gmail.com>
2024-12-10 11:20:39 +0000

    Version #2 - Questions 50%
    10 out of 20 questions are already written on the Java Script code.

--- js/questions.js
+++ js/questions.js
@@ -1,57 +1,118 @@
 let questions = [
     {
     numb: 1,
-    question: "Q1",
-    answer: "A2",
+    question: "Qual é o nome do protagonista da franquia Indiana Jones?",
+    answer: "Dr. Henry Walton Jones Jr.",
     options: [
-      "A1",
-      "A2",
-      "A3",
-      "A4"
+      "Peter Parker",
+      "Dr. Henry Walton Jones Jr.",
+      "Harry Potter",
+      "Henry Cavill"
     ]
   },
     {
     numb: 2,
-    question: "Q2",
-    answer: "A4.2",
+    question: "Em que ano foi lançado o filme <i>Titanic<i>, dirigido por James Cameron?",
+    answer: "1997",
     options: [
-      "A1.2",
-      "A2.2",
-      "A3.2",
-      "A4.2"
+      "1998",
+      "1980",
+      "1987",
+      "1997"
     ]
   },
     {
     numb: 3,
-    question: "Q3",
-    answer: "A1.3",
+    question: "Qual o famoso vilão do cinema que usa uma máscara preta e tem a respiração pesada?",
+    answer: "Darth Vader",
     options: [
-      "A1.3",
-      "A2.3",
-      "A3.3",
-      "A4.3"
+      "Máscara Negra",
+      "Darth Vader",
+      "Jason",
+      "Venom"
     ]
   },
     {
     numb: 4,
-    question: "Q4",
-    answer: "A4.4",
+    question: "Qual é o nome da princesa em A Bela e a Fera?",
+    answer: "Bela",
     options: [
-      "A1.4",
-      "A2.4",
-      "A3.4",
-      "A4.4"
+      "Cinderela",
+      "Aurora",
+      "Bela",
+      "Fera"
     ]
   },
     {
     numb: 5,
-    question: "Q5",
-    answer: "A1.5",
+    question: "Quem interpretou o Coringa no filme O Cavaleiro das Trevas?",
+    answer: "Heath Ledger",
     options: [
-      "A1.5",
-      "A2.5",
-      "A3.5",
-      "A4.5"
+      "Heath Ledger",
+      "Joaquin Phoenix",
+      "Ryan Gosling",
+      "Russell Crowe"
     ]
   },
+
+  {
+    numb: 6,
+    question: "Qual diretor é conhecido como o 'mestre do suspense'?",
+    answer: " Alfred Hitchcock",
+    options: [
+      "Christopher Nolan",
+      "Steven Spielberg",
+      "Alfred Hitchcock",
+      "Tim Burton"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Qual foi o primeiro longa de animação lançado pela Disney?",
+    answer: "Branca de Neve e os Sete Anões",
+    options: [
+      "As aventuras de Peter Pan",
+      "Cinderela",
+      "Hércules",
+      "Branca de Neve e os Sete Anões"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Que filme ganhou o Oscar de Melhor Filme em 2020?",
+    answer: "Parasita",
+    options: [
+      "Oppenheimer",
+      "Parasita",
+      "Pobres Criaturas",
+      "Green Book - Um Guia para a Vida"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Em <i>O Senhor dos Anéis<i>, qual é o nome da montanha onde o Anel foi forjado?",
+    answer: "Montanha da Perdição",
+    options: [
+      "Montanha da Perdição",
+      "Montanha de Denali",
+      "Montanha da Maldição",
+      "Montanha dos Perdidos"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "Em qual planeta se passa a maior parte do filme <i>Duna<i>?",
+    answer: "Arrakis",
+    options: [
+      "Zaun",
+      "Arkanis",
+      "Dunna",
+      "Arrakis"
+    ]
+  },
+
 ];
